d58209cbd3ff944790587945679675d4aed8bb81
Sahil Budhwar <sahilbudhwar143@gmail.com>
2024-04-09 10:23:04 +0530

    configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  {
+    ignores: [
+      "*.config.{cjs,js,mjs}",
+      "node_modules/*",
+      "static/*",
+      "*/dist/*",
+      "@types/*",
+      ".eslintrc.js",
+    ],
+  },
 ];
